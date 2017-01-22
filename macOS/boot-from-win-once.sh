c78c3ab0e83b6a0cd348ef48e6cc1cab23d5ad0e
Ted <littlepig10@users.noreply.github.com>
2017-01-23 00:13:26 +0800

    One more time, but with feeling.
    .

--- macOS/boot-from-win-once.sh
+++ macOS/boot-from-win-once.sh
@@ -3,4 +3,5 @@
 
 # tell OSX to change the boot disk 
 # nextonly just for the next time - without the default would be Windows
+# will fail if SIP is enabled
 /usr/sbin/bless -mount /Volumes/BOOTCAMP -setBoot  --nextonly
